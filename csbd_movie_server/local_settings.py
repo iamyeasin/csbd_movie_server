58d0dbec69ab0fe49e0405d0fbeec220408c3730
yeasin Rahaman <ryeashin@gmail.com>
2019-03-17 03:05:21 +0600

    Update local_settings.py

--- csbd_movie_server/local_settings.py
+++ csbd_movie_server/local_settings.py
@@ -1,1 +1,21 @@
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '084*v1ufo2d2qiv-u=fj=$jlsjg!xq+#((5zm=pe_)w(3q=6n#'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['103.83.15.87']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'csbd_movie_server',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+
